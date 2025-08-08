28aa04429721e2f215cb756a9068639ac408c723
KQuiggins <kashfamilyvalues@gmail.com>
2025-08-08 09:40:55 -0400

    Refactor NoteForm to use a single state object for form data and implement form submission handling

--- forms-app/src/components/NoteForm.jsx
+++ forms-app/src/components/NoteForm.jsx
@@ -1,14 +1,49 @@
 import { useState } from "react";
 
 const NoteForm = () => {
-    const [title, setTitle] = useState("");
-    const [priority, setPriority] = useState("Medium");
-    const [category, setCategory] = useState("Work");
-    const [description, setDescription] = useState("");
+
+    const [formData, setFormData] = useState({
+        title: "",
+        priority: "Medium",
+        category: "Work",
+        description: ""
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+
+
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
 
 
     return (
-        <form className="mb-6">
+        <form className="mb-6" onSubmit={handleSubmit}>
+            <div className="mb-4">
+                <label
+                    htmlFor="Title"
+                    className="block semi-bold"
+                >
+                Title
+                </label>
+                <input
+                    type="text"
+                    className="w-full p-2 border rounded-lg"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                >
+
+                </input>
+            </div>
             <div className="mb-4">
                 <label
                     htmlFor="priority"
@@ -17,16 +52,52 @@ const NoteForm = () => {
                 Priority
                 </label>
                 <select
-                    type="text"
+                    name="priority"
                     className="w-full p-2 border rounded-lg"
-                    value={priority}
-                    onChange={(e) => setPriority(e.target.value)}
+                    value={formData.priority}
+                    onChange={handleChange}
                 >
-                    <option value="High">High</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Low">Low</option>
+                    <option value="High">🔴 High</option>
+                    <option value="Medium">🔴 Medium</option>
+                    <option value="Low">🔴 Low</option>
                 </select>
             </div>
+            <div className="mb-4">
+                <label
+                    htmlFor="category"
+                    className="block semi-bold"
+                >
+                Category
+                </label>
+                <select
+                    name="category"
+                    className="w-full p-2 border rounded-lg"
+                    value={formData.category}
+                    onChange={handleChange}
+                >
+                    <option value="Work">Work</option>
+                    <option value="Personal">Personal</option>
+                    <option value="Ideas">Ideas</option>
+                </select>
+            </div>
+            <div className="mb-4">
+                <label
+                    htmlFor="description"
+                    className="block semi-bold"
+                >
+                Description
+                </label>
+                <textarea
+                    name="description"
+                    type="text"
+                    className="w-full p-2 border rounded-lg"
+                    value={formData.description}
+                    onChange={handleChange}
+                >
+
+                </textarea>
+            </div>
+            <button className="w-full text-white bg-purple-500 hover:bg-purple-600 py-2 rounded-lg cursor-pointer">Add Note</button>
         </form>
      );
 }
