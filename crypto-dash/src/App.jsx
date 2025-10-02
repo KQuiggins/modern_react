44463eaf5b8ab4bd9c95ab68dc5b62f8c8753f98
KQuiggins <kashfamilyvalues@gmail.com>
2025-10-02 13:34:06 -0400

    feat: refactor data fetching to use async/await for improved readability

--- crypto-dash/src/App.jsx
+++ crypto-dash/src/App.jsx
@@ -7,30 +7,57 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((response) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        return response.json();
-      })
-      .then((data) => {
+        const data = await response.json();
         console.log(data);
+
         setCoins(data);
-        setLoading(false);
-      })
-      .catch((error) => {
+      } catch (error) {
         setError(error.message);
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+    fetchData();
   }, []);
+
+
+
 
 
   return (
     <div>
       <h1>🚀🧑‍🚀 Crypto Dashboard</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {!loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <div className="coin-card" key={coin.id}>
+              <div className="coin-header">
+                <img src={coin.image} alt={coin.name} className="coin-image" />
+                <div>
+                  <h2>{coin.name}</h2>
+                  <span className="symbol">{coin.symbol.toUpperCase()}</span>
+                </div>
+
+              </div>
+              <p>Price: ${coin.current_price}</p>
+              <p className={coin.price_change_percentage_24h >= 0 ? "positive" : "negative"}>
+                {coin.price_change_percentage_24h.toFixed(2)}% 24h
+              </p>
+              <p>Market Cap: ${coin.market_cap.toLocaleString()}</p>
+            </div>
+          ))}
+        </main>
+      )}
     </div>
   );
-}
+};
 
 export default App;
