6b92f03d5f5239cc8369979e8d93af0a80f77fcf
KQuiggins <kashfamilyvalues@gmail.com>
2026-05-11 19:55:14 -0400

    feat: add mobile menu functionality to Navbar component

--- friendy-dev/app/components/Navbar.tsx
+++ friendy-dev/app/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router";
-import { FaLaptopCode } from "react-icons/fa6";
+import { FaLaptopCode, FaXmark, FaBars } from "react-icons/fa6";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="bg-gray-900/80 backdrop-blur border-b border-gray-800 shadow-sm sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
@@ -35,8 +38,53 @@ const Navbar = () => {
             );
           })}
         </div>
+        {/* Mobile Menu Button */}
+        <div className="md:hidden flex items-center gap-4">
+          <button
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="text-blue-400 text-xl cursor-pointer"
+            title="Menu"
+          >
+            {isMenuOpen ? <FaXmark /> : <FaBars />}
+          </button>
+        </div>
+
 
       </div>
+      {/* Mobile Nav */}
+        { isMenuOpen && (
+          <div className="
+            bg-gray-800
+            border-t
+            border-gray-700
+            px-6
+            py-4
+            space-y-2
+            space-x-4
+            text-center
+            md:hidden
+            ">
+              {["/", "/projects", "/blog", "/about", "/contact"].map((path, i) => {
+            const labels = ["Home", "Projects", "Blog", "About", "Contact"];
+            return (
+              <NavLink
+                key={path}
+                to={path}
+                onClick={() => {setIsMenuOpen(false)}}
+                className={({ isActive }) =>
+                  `transition-colors duration-200 ${
+                    isActive
+                      ? "text-white border-b-2 border-blue-400 pb-1"
+                      : "text-gray-400 hover:text-white"
+                  }`
+                }
+              >
+                {labels[i]}
+              </NavLink>
+            );
+          })}
+          </div>
+        )}
     </nav>
   );
 };
